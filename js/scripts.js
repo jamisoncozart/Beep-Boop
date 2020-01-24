d68eb2831e9c59318c2a304e9f6db7fb7aa8c96b
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-24 09:32:49 -0800

    Handle non-numeric user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
+//User Interface Logic
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
     var input = $("#userInput").val();
-    console.log(input);
+    $("#output").text(createOutput(input));
   })
 })
+
+//Backend Logic
+function createOutput(input) {
+  return "Error: Please enter a number";
+}
