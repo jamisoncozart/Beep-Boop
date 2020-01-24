35862e0d4901e6bc59de14d71908cf3c423341d1
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-24 09:56:40 -0800

    Add contains1() function to check if each list number contains a '1'

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
-//User Interface Logic
+//===================================================
+//Front-End Logic
+//---------------------------------------------------
+
+//Runs all front-end logic once page has loaded.
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
@@ -7,13 +11,22 @@ $(document).ready(function() {
   })
 })
 
-//Backend Logic
+//===================================================
+//Back-End Logic
+//---------------------------------------------------
+
+//creates output based on conditionals and returns output string.
 function createOutput(input) {
   if(Number.isInteger(input)) {
     var listString = "";
     for(let i = 0; i <= input; i++) {
       if(i === input) {
+        if(contains1(i)) {
+          listString += "Beep!"
+        }
         listString += i;
+      } else if(contains1(i)) {
+        listString += "Beep!, ";
       } else {
         listString += (i + ", ");
       }
@@ -22,4 +35,15 @@ function createOutput(input) {
   } else {
     return "Error: Please enter a number";
   }
+}
+
+//converts number input to string and reutnr true if it contains a "1", else returns false.
+function contains1(input) {
+  var numString = input.toString();
+  for(let i = 0; i < numString.length; i++) {
+    if(numString[i] === "1") {
+      return true;
+    }
+  }
+  return false;
 }
