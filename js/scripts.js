cce9ffff39f548fc62ea57cb7e6fdee818438f6a
Jamison Cozart <jamisoncozart@gmail.com>
2020-01-24 09:42:34 -0800

    List all numbers from 0 to user input number on submission

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,24 @@
 $(document).ready(function() {
   $("#form").submit(function(event) {
     event.preventDefault();
-    var input = $("#userInput").val();
+    var input = parseInt($("#userInput").val());
     $("#output").text(createOutput(input));
   })
 })
 
 //Backend Logic
 function createOutput(input) {
-  return "Error: Please enter a number";
+  if(Number.isInteger(input)) {
+    var listString = "";
+    for(let i = 0; i <= input; i++) {
+      if(i === input) {
+        listString += i;
+      } else {
+        listString += (i + ", ");
+      }
+    }
+    return listString;
+  } else {
+    return "Error: Please enter a number";
+  }
 }
